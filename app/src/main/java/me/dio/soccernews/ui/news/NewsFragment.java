3c908c08c19d426aa998cc3ce55c03f95803b1c8
Perles <luanc.mendes@hotmail.com>
2022-06-17 17:36:44 -0300

    Inclusão do Retrofit e Picasso

--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -4,15 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import me.dio.soccernews.databinding.FragmentNewsBinding;
-import me.dio.soccernews.databinding.FragmentNewsBinding;
 import me.dio.soccernews.ui.news.adapter.MeusAdapter;
 
 public class NewsFragment extends Fragment {
